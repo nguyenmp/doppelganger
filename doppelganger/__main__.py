dd261d0c6fc938f77f4b187633df835e04658dbe
Mark Nguyen <mnguyen4@apple.com>
2019-02-03 22:40:47 -0800

    Write to file and open image per user

--- doppelganger/__main__.py
+++ doppelganger/__main__.py
@@ -2,13 +2,27 @@
 This is the main module.  It is executed when run with `python -m doppleganger`
 '''
 
+import base64
 import json
+import subprocess
 
 from . import (
     init_ldap,
     get_employees,
     logger,
 )
+
+
+def save_bytes_to_file(byte_array):
+    '''
+    Given an array of bytes, returns the filename of a
+    a temporary file that contains those contents
+    '''
+    import tempfile
+    handle = tempfile.NamedTemporaryFile(mode='w+b', delete=False)
+    handle.write(byte_array)
+    handle.flush()
+    return handle.name
 
 
 def main():
@@ -19,12 +33,11 @@ def main():
     for employee in get_employees(ldap_instance):
         json.dumps(employee)
         logger.info('Looking at %s', json.dumps(employee))
-        # handle = tempfile.NamedTemporaryFile(mode='w+b', suffix='.jpeg')
-        # handle.write(image_data)
-        # handle.flush()
-        # subprocess.check_call('open {}'.format(handle.name), shell=True)
-        # import pdb
-        # pdb.set_trace()
+        image_bytes = base64.b64decode(employee['applePhotoOfficial-jpeg'])
+        file_name = save_bytes_to_file(image_bytes)
+        subprocess.check_call('open {}'.format(file_name), shell=True)
+        import pdb
+        pdb.set_trace()
 
 
 if __name__ == '__main__':
